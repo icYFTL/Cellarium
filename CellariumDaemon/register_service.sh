9c102342bf87fa2db50cb5f4e8f376a559a85144
icYFTL <savap0@yandex.ru>
2023-01-23 00:06:47 +0300

    hotfixes #2

--- CellariumDaemon/register_service.sh
+++ CellariumDaemon/register_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -d "/etc/systemd/system" ]; then
-  mv ./cellarium_daemon.service /etc/systemd/system
+  mv /usr/share/cellarium/daemon/cellarium_daemon.service /etc/systemd/system
 else
   echo "Directory /etc/systemd/system does not exist. Can\'t register a cellarium daemon service."
 fi
